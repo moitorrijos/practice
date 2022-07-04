b979c877617a333ce1c9660dc0959f57a776e0ca
Juan Moises Torrijos <juanmtorrijos@gmail.com>
2022-07-04 17:35:31 -0500

    Adds README.

---
+++ README.md
@@ -0,0 +1,1 @@
+_palindrome.js_ is a function that takes in a word and checks if is a function.
--- palindrome.js
+++ palindrome.js
@@ -1,8 +1,13 @@
+/**
+ * Checks if a word is a palindrome
+ * @param {string} str - a word to check if is a palindrome
+ * @returns {string}
+ */
 function palindrome(str) {
   if (typeof str !== "string") throw new Error(`${str} must be a string.`)
-  const reverseStr = str.split('').reverse().join('')
-  if (str === reverseStr) { return `${str} is a palindrome.` }
+  const reverseStr = strArray.reverse().join('').toLowerCase()
+  if (str.toLowerCase() === reverseStr) return `${str} is a palindrome.`
   return `${str} is not a palindrome.`
 }
-console.log(palindrome("ana")) // Ana is a palindrome.
-console.log(palindrome("panama")) // Panama is not a palindrome.
+console.log(palindrome("Hannah")) // Hannah is a palindrome.
+console.log(palindrome("Panama")) // Panama is not a palindrome.
