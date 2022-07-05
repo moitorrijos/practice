8b302c9428f9b0393b01852971bf5d8829225447
Juan Moises Torrijos <juanmtorrijos@gmail.com>
2022-07-04 19:53:02 -0500

    Improves fastPalindrome.js

--- fastPalindrome.js
+++ fastPalindrome.js
@@ -1,18 +1,19 @@
-function checkFirstLastChar(strArray) {
+function checkPalindrome(strArray) {
   if (strArray[0] !== strArray[strArray.length - 1]) {
-    return `is not a palindrome.`
+    return false
   }
   strArray.shift()
   strArray.pop()
   if (strArray.length > 1) {
-    checkFirstLastChar(strArray)
+    checkPalindrome(strArray)
   }
-  return 'is a palindrome.'
+  return true
 }
 function fastPalindrome(str) {
   if (typeof str !== "string") throw new Error(`${str} must be a string.`)
   const strArray = str.toLowerCase().split('')
-  return str + ' ' + checkFirstLastChar(strArray)
+  if ( checkPalindrome(strArray) ) return `${str} is a palindrome.`
+  return `${str} is not a palindrome.`
 }
 console.log(fastPalindrome("Hannah")) // Hannah is a palindrome.
 console.log(fastPalindrome("Panama")) // Panama is not a palindrome.
